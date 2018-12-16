ae11088dbf61664e3dfb14acc62f1b3cb8561d91
deepakgarg0802 <deepakgarg0802@gmail.com>
2018-12-16 22:19:46 +0530

    Add : UserAccount Controllers added

--- src/main/java/com/example/ewallet/dataaccessrepository/UserAccountRepository.java
+++ src/main/java/com/example/ewallet/dataaccessrepository/UserAccountRepository.java
@@ -1,9 +1,12 @@
 package com.example.ewallet.dataaccessrepository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.ewallet.models.UserAccount;
 
 public interface UserAccountRepository extends CrudRepository<UserAccount, Long>{
 
+	Optional<UserAccount> getByUserName(String name);
 }
---
+++ src/main/java/com/example/ewallet/walletapp/controller/UserAccountController.java
@@ -0,0 +1,93 @@
+package com.example.ewallet.walletapp.controller;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ewallet.datatransferobject.TransactionDTO;
+import com.example.ewallet.datatransferobject.UserAccountDTO;
+import com.example.ewallet.datatransferobject.mapper.TransactionMapper;
+import com.example.ewallet.datatransferobject.mapper.UserAccountMapper;
+import com.example.ewallet.models.Transaction;
+import com.example.ewallet.models.UserAccount;
+import com.example.ewallet.service.TransactionService;
+import com.example.ewallet.service.UserAccountService;
+
+@RestController
+@RequestMapping("v1/users")
+public class UserAccountController {
+
+	@Autowired
+	private UserAccountService userAccountService;
+
+	@Autowired
+	private TransactionService walletService;
+
+	@GetMapping
+	public ResponseEntity getUsers() {
+		List<UserAccount> userAccounts = userAccountService.getList();
+		return new ResponseEntity<List<UserAccountDTO>>(UserAccountMapper.doToDTOList(userAccounts), HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity getUser(@PathVariable("id") Long id) {
+		UserAccount userAccount;
+		try {
+			userAccount = userAccountService.userAccountByPK(id);
+		} catch (Exception ex) {
+			Logger.getLogger(UserAccountController.class.getName()).log(Level.SEVERE, null, ex);
+			return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<UserAccountDTO>(UserAccountMapper.doToDTO(userAccount), HttpStatus.OK);
+
+	}
+
+	@PostMapping
+	public ResponseEntity createUser(@RequestBody UserAccountDTO userAccountDTO) {
+		UserAccount saved;
+		try {
+			saved = userAccountService.save(UserAccountMapper.dtoToDO(userAccountDTO));
+		} catch (Exception ex) {
+			Logger.getLogger(UserAccountController.class.getName()).log(Level.SEVERE, null, ex);
+			return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<UserAccountDTO>(UserAccountMapper.doToDTO(saved), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity updateUser(@PathVariable("id") Long userAccountId, @RequestBody UserAccountDTO userAccountDTO) {
+		UserAccount saved;
+		try {
+			saved = userAccountService.update(UserAccountMapper.dtoToDO(userAccountDTO), userAccountId);
+		} catch (Exception ex) {
+			Logger.getLogger(UserAccountController.class.getName()).log(Level.SEVERE, null, ex);
+			return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<UserAccountDTO>(UserAccountMapper.doToDTO(saved), HttpStatus.OK);
+	}
+
+	@PostMapping("/{id}/transactions")
+	public ResponseEntity createTransaction(@PathVariable("id")Long userAccountId,@RequestBody TransactionDTO walletDTO) {
+		Transaction saved;
+		try {
+			walletDTO.setUserAccountId(userAccountId);
+			saved = walletService.createTransaction(TransactionMapper.dtoToDO(walletDTO));
+		} catch (Exception ex) {
+			Logger.getLogger(UserAccountController.class.getName()).log(Level.SEVERE, null, ex);
+			return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+		} 
+		return new ResponseEntity<TransactionDTO>(TransactionMapper.doToDTO(saved), HttpStatus.CREATED);
+	}
+
+}
